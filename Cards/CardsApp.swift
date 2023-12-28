f50cba40bbd17fea5688e2fd609c686a6b0c909a
pavel610 <78311696+pavel610@users.noreply.github.com>
2023-12-28 11:20:48 +0300

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Павел Калинин on 28.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
